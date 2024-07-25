72563d85a430b9c0df16b1cc7d47ce55c0be6d6e
andrew-Jolly <andy_jolly84@icloud.com>
2024-07-25 12:49:56 +0100

    add ng primitives and basic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,71 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NgpDescription, NgpFormControl, NgpFormField, NgpLabel } from 'ng-primitives/form-field';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [
+        RouterOutlet,
+        NgpFormField,
+        NgpLabel,
+        NgpDescription,
+        NgpFormControl,
+        ReactiveFormsModule
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+    postcode = new FormControl<string>('');
+
+    // mock response
+    // {
+    //     "status": 200,
+    //     "result": {
+    //         "postcode": "BT53 7HX",
+    //         "quality": 1,
+    //         "eastings": 294866,
+    //         "northings": 424744,
+    //         "country": "Northern Ireland",
+    //         "nhs_ha": "Health & Social Care Board",
+    //         "longitude": -6.516078,
+    //         "latitude": 55.060991,
+    //         "european_electoral_region": "Northern Ireland",
+    //         "primary_care_trust": "Northern",
+    //         "region": null,
+    //         "lsoa": "Seacon",
+    //         "msoa": null,
+    //         "incode": "7HX",
+    //         "outcode": "BT53",
+    //         "parliamentary_constituency": "North Antrim",
+    //         "parliamentary_constituency_2024": "North Antrim",
+    //         "admin_district": "Causeway Coast and Glens",
+    //         "parish": null,
+    //         "admin_county": null,
+    //         "date_of_introduction": "200411",
+    //         "admin_ward": "Route",
+    //         "ced": null,
+    //         "ccg": "Northern",
+    //         "nuts": "Causeway Coast and Glens",
+    //         "pfa": "(pseudo) Northern Ireland",
+    //         "codes": {
+    //             "admin_district": "N09000004",
+    //             "admin_county": "N99999999",
+    //             "admin_ward": "N08000436",
+    //             "parish": "N99999999",
+    //             "parliamentary_constituency": "N06000012",
+    //             "parliamentary_constituency_2024": "N05000012",
+    //             "ccg": "ZC020",
+    //             "ccg_id": "ZC020",
+    //             "ced": "N99999999",
+    //             "nuts": "TLN0C",
+    //             "lsoa": "95EE14W1",
+    //             "msoa": "N99999999",
+    //             "lau2": "N09000004",
+    //             "pfa": "N99999999"
+    //         }
+    //     }
+    // }
+}
