b2713e53571292cad4f699d7bc225af8bd36a7b8
andrew-Jolly <andy_jolly84@icloud.com>
2024-07-26 09:47:53 +0100

    add interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,13 +24,58 @@ import { map, tap } from 'rxjs';
 export class AppComponent implements OnInit {
     postcode = new FormControl<string>('');
 
+    mockData = {
+        "postcode": "BT53 7HX",
+        "quality": 1,
+        "eastings": 294866,
+        "northings": 424744,
+        "country": "Northern Ireland",
+        "nhs_ha": "Health & Social Care Board",
+        "longitude": -6.516078,
+        "latitude": 55.060991,
+        "european_electoral_region": "Northern Ireland",
+        "primary_care_trust": "Northern",
+        "region": null,
+        "lsoa": "Seacon",
+        "msoa": null,
+        "incode": "7HX",
+        "outcode": "BT53",
+        "parliamentary_constituency": "North Antrim",
+        "parliamentary_constituency_2024": "North Antrim",
+        "admin_district": "Causeway Coast and Glens",
+        "parish": null,
+        "admin_county": null,
+        "date_of_introduction": "200411",
+        "admin_ward": "Route",
+        "ced": null,
+        "ccg": "Northern",
+        "nuts": "Causeway Coast and Glens",
+        "pfa": "(pseudo) Northern Ireland",
+        "codes": {
+            "admin_district": "N09000004",
+            "admin_county": "N99999999",
+            "admin_ward": "N08000436",
+            "parish": "N99999999",
+            "parliamentary_constituency": "N06000012",
+            "parliamentary_constituency_2024": "N05000012",
+            "ccg": "ZC020",
+            "ccg_id": "ZC020",
+            "ced": "N99999999",
+            "nuts": "TLN0C",
+            "lsoa": "95EE14W1",
+            "msoa": "N99999999",
+            "lau2": "N09000004",
+            "pfa": "N99999999"
+        }
+    }
+
     constructor(private httpClient: HttpClient) {}
 
     ngOnInit(): void {
         // TODO - add search service
         const endpoint = 'https://postcodes.io/postcodes/BT53 7HX'
-        this.httpClient.get(encodeURI(endpoint)).pipe(
-            map((res: any) => res.result)
+        this.httpClient.get<SearchResult>(encodeURI(endpoint)).pipe(
+            map((res: SearchResult) => res.result)
         ).subscribe({
             next: (res) => console.log(res),
             error: (error) => console.log(error)
@@ -85,4 +130,56 @@ export class AppComponent implements OnInit {
     //         }
     //     }
     // }
+}
+
+interface PostcodeData {
+    postcode: string;
+    quality: number;
+    eastings: number;
+    northings: number;
+    country: string;
+    nhs_ha: string;
+    longitude: number;
+    latitude: number;
+    european_electoral_region: string;
+    primary_care_trust: string;
+    region: string;
+    lsoa: string;
+    msoa: string;
+    incode: string;
+    outcode: string;
+    parliamentary_constituency: string;
+    parliamentary_constituency_2024: string;
+    admin_district: string;
+    parish: string;
+    admin_county: string;
+    date_of_introduction: string;
+    admin_ward: string;
+    ced: string;
+    ccg: string;
+    nuts: string;
+    pfa: string;
+    codes: PostcodeDataCodes;
+}
+
+interface PostcodeDataCodes {
+    admin_district: string;
+    admin_county: string;
+    admin_ward: string;
+    parish: string;
+    parliamentary_constituency: string;
+    parliamentary_constituency_2024: string;
+    ccg: string;
+    ccg_id: string;
+    ced: string;
+    nuts: string;
+    lsoa: string;
+    msoa: string;
+    lau2: string;
+    pfa: string;
+}
+
+interface SearchResult {
+    status: number;
+    result: PostcodeData;
 }
